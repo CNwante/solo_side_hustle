26eaff73b993c40b9a2560a3d87b70456ffb7336
CNwante <myphilothea@gmail.com>
2023-05-21 14:01:25 -0700

    File: add githubui.js

---
+++ Udemy-Modern_JavaScript_From_The_Beginning/05-github_user_finder/githubui.js
@@ -0,0 +1,92 @@
+// Cretae UI class
+
+class UI {
+    constructor() {
+        this.profile = document.getElementById('profile');
+    }
+
+    // Create shoeProfile method
+    showProfile(user) {
+        this.profile.innerHTML = `
+        <div class="card card-body mb-3">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-fluid md-2 mb-2" src="${user.avatar_url}">
+                    <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                </div>
+                <div class=col-md-9>
+                    <span class="badge badge-primary mb-2">Public Repos: ${user.public_repos}</span>
+                    <span class="badge badge-primary mb-2">Public Gists: ${user.public_gists}</span>
+                    <span class="badge badge-primary mb-2">Followers: ${user.followers}</span>
+                    <span class="badge badge-primary mb-2">Following: ${user.following}</span>
+                    <br> <br>
+                    <ul class="list-group">
+                        <li class="list-group-item">Company: ${user.company}</li>
+                        <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                        <li class="list-group-item">Location: ${user.location}</li>
+                        <li class="list-group-item">Created at: ${user.created_at}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <h3 class="page-heading mb-3">Latest Repos:</h3>
+        <div id="repos"></div>
+        `;
+    }
+    // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    for (let repo of repos) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    }
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+    // Show Error message
+    showMessage(message, className) {
+        this.clearAlert();
+        // Create div Element
+        const div = document.createElement('div');
+        // Add class name
+        div.className = className;
+        // Add message
+        div.appendChild(document.createTextNode(message));
+        // Select container and insert
+        const container = document.querySelector('.searchContainer');
+        const search = document.querySelector('.search');
+        container.insertBefore(div, search);
+
+        // Set time out for clear alert
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+    // Clear alert messages
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    // Create clear profile method
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
+}
